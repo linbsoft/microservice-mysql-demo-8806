cf3e3863d6ef0e766b01853c684847d925c228f0
linbsoft <gdcxmail@163.com>
2019-01-15 00:27:50 +0800

    spring cloud mysql demo

---
+++ README.md
@@ -0,0 +1,1 @@
+# microservice-mysql-demo-8806
---
+++ src/main/java/com/linbsoft/microservicemysqldemo8806/MicroserviceMysqlDemo8806Application.java
@@ -0,0 +1,14 @@
+package com.linbsoft.microservicemysqldemo8806;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMysqlDemo8806Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMysqlDemo8806Application.class, args);
+	}
+
+}
+
